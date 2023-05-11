bea999d5b311bea9da2345c10c96df06251dbd0e
KlarkC <walkerleite490@gmail.com>
2023-05-11 18:53:38 -0300

    Revert "feat: change to purifix to allow purenix"
    This reverts commit 8b104e58b5edf1968c1d0fc2be125e2d20a48214.

--- flake.nix
+++ flake.nix
@@ -1,21 +1,51 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    purifix.url = "github:purifix/purifix";
+    utils.url = "github:ursi/flake-utils";
+    purs-nix.url = "github:purs-nix/purs-nix";
+    ps-tools.follows = "purs-nix/ps-tools";
+    nixpkgs.follows = "purs-nix/nixpkgs";
   };
 
-  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import inputs.nixpkgs {
-        inherit system;
-        overlays = [ inputs.purifix.overlay ];
-      };
-      os = pkgs.purifix {
-        src = ./.;
-      };
-    in
-    {
-      packages.default = os;
-    });
+  outputs = { self, utils, ... }@inputs:
+    utils.apply-systems
+      {
+        inherit inputs;
+        make-pkgs = system: import inputs.nixpkgs {
+          inherit system;
+          # enable ca-derivations globally
+          config.contentAddressedByDefault = true;
+        };
+      }
+      ({ pkgs, system, ... }:
+        let
+          purs-nix = inputs.purs-nix { inherit system; };
+          ps = purs-nix.purs
+            {
+              # Project dir (src, test)
+              dir = ./.;
+              # Dependencies
+              dependencies =
+                with purs-nix.ps-pkgs;
+                [
+                  prelude
+                  console
+                  effect
+                ];
+            };
+          ps-command = ps.command { };
+        in
+        {
+          packages.default = ps.output { };
+
+          devShells.default =
+            pkgs.mkShell
+              {
+                packages =
+                  with pkgs;
+                  [
+                    ps-command
+                    ps-tools.for-0_15.purescript-language-server
+                  ];
+              };
+        });
 }
