7fbd643d67e9b4837fffda92970ec429e76ad848
klarkc <walkerleite490@gmail.com>
2023-05-25 12:07:15 -0300

    feat: add users and networking modules

--- src/Main.purs
+++ src/Main.purs
@@ -1,20 +1,20 @@
 module Main where
 
-type Inputs a b c r0 r1 r2 r3 r4
+type Inputs a r0 r1 r2 r3 r4
   = { nixpkgs ::
         { lib ::
             { nixosSystem ::
-                { system :: a
-                , modules :: Array b
+                { system :: System
+                , modules :: Array Module
                 } ->
-                c
+                a
             | r0
             }
         | r1
         }
     , microvm ::
         { nixosModules ::
-            { microvm :: b
+            { microvm :: MicrovmModule
             | r3
             }
         | r4
@@ -22,16 +22,44 @@ type Inputs a b c r0 r1 r2 r3 r4
     | r2
     }
 
-type Context a b
-  = { system :: a
-    , pkgs :: { microvm :: { nixosModules :: { microvm :: b } } }
+type Context
+  = { system :: System
+    , pkgs :: { microvm :: { nixosModules :: { microvm :: MicrovmModule } } }
     }
 
-main :: forall a b c r0 r1 r2 r3 r4. Inputs a b c r0 r1 r2 r3 r4 -> Context a b -> c
+data MicrovmModule
+
+data System
+
+type NetworkingModule
+  = { networking :: { hostName :: String } }
+
+type UsersModule
+  = { users :: { users :: { root :: { password :: String } } } }
+
+data Module
+  = Microvm MicrovmModule
+  | Networking NetworkingModule
+  | Users UsersModule
+
+fromModule :: forall a. Module -> a
+fromModule (Microvm mod) = mod
+fromModule (Networking mod) = mod
+fromModule (Users mod) = mod
+
+networking :: NetworkingModule
+networking = { networking: { hostName: "klarkc-os" } }
+
+users :: UsersModule
+users = { users: { users: { root: { password: "1234" } } } }
+
+main :: forall r0 r1 r2 r3 r4. Inputs a r0 r1 r2 r3 r4 -> Context -> a
 main inputs ctx =
   inputs.nixpkgs.lib.nixosSystem
     { system: ctx.system
     , modules:
-        [ inputs.microvm.nixosModules.microvm
+        [ fromModule (Microvm inputs.microvm.nixosModules.microvm)
+        , fromModule (Networking networking)
+        , fromModule (Users users)
         ]
     }
