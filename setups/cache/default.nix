aee7c91e8c0666a1608ba1b30dd62b587f6dfd9f
klarkc <walkerleite490@gmail.com>
2023-12-20 16:11:47 -0300

    refact(cache): split virt opts to cache-vm

--- setups/cache/default.nix
+++ setups/cache/default.nix
@@ -1,6 +1,5 @@
 { system, pkgs, flake, ... }:
 let
-  inherit (flake.inputs.generators.nixosModules) vm-nogui;
   inherit (flake.inputs.everyday.nixosModules) logger host-keys;
   inherit (flake.outputs.lib) mkSystem secrets;
   agenix = flake.inputs.agenix.nixosModules.default;
@@ -8,22 +7,18 @@ let
   domain = "wcasa.wifizone.org";
   home = "/home/klarkc";
   cache-module = { config, ... }:
-    let
-      inherit (config.services.nix-serve) port;
-    in
     {
-      imports = [ logger nix-serve vm-nogui agenix host-keys ];
+      imports = [ logger nix-serve agenix ];
       # cd secrets
       # nix-store --generate-binary-cache-key wcasa.wifizone.org ./cache ./cache.skey
       # cat cache | nix run github:ryantm/agenix -- -e cache.age -i ~/.ssh/id_ed25519
       # cp ~/.ssh/id_ed25519.pub klarkc.pub
       age.secrets.cache.file = "${secrets}/cache.age";
-      host-keys.source = "${home}/.ssh";
       system.stateVersion = config.system.nixos.version;
       fileSystems."/".device = "none";
       boot.loader.grub.device = "nodev";
       services = {
-        sshd.enable =true;
+        sshd.enable = true;
         nix-serve = {
           enable = true;
           secretKeyFile = config.age.secrets.cache.path;
@@ -41,10 +36,6 @@ let
         mutableUsers = false;
       };
       networking.firewall.enable = false;
-      virtualisation.forwardPorts = [
-        { from = "host"; host.port = 2222; guest.port = 22; }
-        { from = "host"; host.port = port; guest.port = port; }
-      ];
       # Web server
       services.nginx = {
         virtualHosts.${domain} = {
@@ -64,7 +55,19 @@ rec {
   modules.cache = cache-module;
 
   packages.cache-vm = mkSystem {
-    modules = with modules; [ cache ];
+    modules = with modules; [
+      cache
+      ({ config, ... }:
+        let inherit (config.services.nix-serve) port; in
+        {
+          imports = [ host-keys ];
+          host-keys.source = "${home}/.ssh";
+          virtualisation.forwardPorts = [
+            { from = "host"; host.port = 2222; guest.port = 22; }
+            { from = "host"; host.port = port; guest.port = port; }
+          ];
+        })
+    ];
     format = "vm-nogui";
   };
 
