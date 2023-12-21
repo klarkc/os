d8471711173ac85e941c4ab1a30e6d548a06f6ec
klarkc <walkerleite490@gmail.com>
2023-12-21 19:29:27 -0300

    feat: change nix cache

--- flake.nix
+++ flake.nix
@@ -65,12 +65,10 @@
     # Nix should ask for permission before using it,
     # but remove it here if you do not want it to.
     extra-substituters = [
-      "https://klarkc.cachix.org?priority=99"
-      "https://cache.nixos.org"
+      "https://cache.tcp4.me"
     ];
     extra-trusted-public-keys = [
-      "klarkc.cachix.org-1:R+z+m4Cq0hMgfZ7AQ42WRpGuHJumLLx3k0XhwpNFq9U="
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "cache.tcp4.me:cmk2Iz81lQuX7FtTUcBgtqgI70E8p6SOamNAIcFDSew="
     ];
   };
 }
