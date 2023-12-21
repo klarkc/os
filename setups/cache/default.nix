5dc7549b7048bc6fda14d2fe1b7d23046b54ef26
klarkc <walkerleite490@gmail.com>
2023-12-21 19:19:30 -0300

    feat(cache): upgrade keys

--- setups/cache/default.nix
+++ setups/cache/default.nix
@@ -16,9 +16,9 @@ let
         disko
       ];
       # cd secrets
-      # nix-store --generate-binary-cache-key cache.tcp4.me ./cache ./cache.skey
-      # cat cache | nix run github:ryantm/agenix -- -e cache.age -i cache-vultr.pub 
+      # nix-store --generate-binary-cache-key cache.tcp4.me ./cache ./cache.pub
       # scp ssh://root@cache.tcp4.me:/etc/ssh/ssh_host_ed25519_key.pub cache-vultr.pub
+      # cat cache | nix run github:ryantm/agenix -- -e cache.age -i cache-vultr.pub 
       age.secrets.cache.file = "${secrets}/cache.age";
       system.stateVersion = config.system.nixos.version;
       boot.loader.systemd-boot.enable = true;
